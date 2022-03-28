762e9a9b12a1df5b1b3295366ba3e8662c3ec6c0
boba2fett <boba2002fett@gmail.com>
2022-03-28 20:40:25 +0200

    Suggested improvements - reorder build steps in Dockerfile to fasten up build with no changes in dependencies - added try catch blocks to increase stability - added # before Error output one should take a closer look at

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM python:3.9
 WORKDIR /usr/src/app
 
 COPY src/requirements.txt .
+RUN pip3 install -r requirements.txt
+
 COPY src/main.py .
 COPY src/db.py .
 COPY src/crawl_news.py .
 COPY src/update_news.py .
-
-RUN pip3 install -r requirements.txt
 
 CMD [ "python3", "main.py" ]
--- src/crawl_news.py
+++ src/crawl_news.py
@@ -5,23 +5,25 @@ import requests
 
 class CrawlNews:
 
-    db = TagesschauDB()
-
     def __init__(self):
+        self.db = TagesschauDB()
+ 
         while True:
             self.crawl_articles()
             sleep(5 * 60)
 
-
     def crawl_articles(self):
-        articles = requests.get('https://www.tagesschau.de/api2').json()['news']
+        try:
+            articles = requests.get('https://www.tagesschau.de/api2').json()['news']
 
-        for index, article in enumerate(articles):
-            if article.get('sophoraId', None):
-                existing_article = self.db.get_article(article['sophoraId'])
+            for index, article in enumerate(articles):
+                if article.get('sophoraId', None):
+                    existing_article = self.db.get_article(article['sophoraId'])
                 
-                if not existing_article:
-                    self.db.insert_article(article, index)
-                else:
+                    if not existing_article:
+                        self.db.insert_article(article, index)
+                        continue
                     if existing_article['article']['crawler']['crawlTime'] < (datetime.now() - timedelta(days=1)):
                         self.db.update_article(article, index, existing_article['article']['crawler']['insertTime'])
+        except Exception as e:
+            print(f'# Failed crawling tagesschau, because of "{e}"')
